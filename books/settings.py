5d5ea893c10fe569e0127dc1c728fd54064f5f4a
darkhanakmyrza <darkhanakmyrza1@gmail.com>
2020-09-08 22:21:24 +0500

    adding store app and database

--- books/settings.py
+++ books/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'store',
 ]
 
 MIDDLEWARE = [
@@ -80,7 +81,6 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
